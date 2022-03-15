f513fed035c86d177c6b20edab657c4958a887aa
NataliaIvanko <95305027+NataliaIvanko@users.noreply.github.com>
2022-03-15 10:33:21 +0100

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Average {
+    int a;
+    int b;
+    int c;
+
+    public Average(int a, int b, int c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double average(){
+        return a*b*c/3;
+    }
+}
